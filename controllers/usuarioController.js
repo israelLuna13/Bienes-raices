e49b91a96b3b558eeef3707451a0e497b86a3603
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-06-07 20:32:27 -0600

    Se agrego la validacion para evitar usuarios duplicados

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -26,6 +26,9 @@ const registrar = async(req,res)=>{
     await check('password').isLength({min:6}).withMessage('El password debe ser de al menos 6 caracteres').run(req)
     await check('repetir_password').equals(req.body.password).withMessage('El password no coincide').run(req)
 
+
+
+
     //resultado de la validacion de arriba
     let resultado = validationResult(req)
 
@@ -41,8 +44,25 @@ const registrar = async(req,res)=>{
             }
         })
     }
-    const usuario = await Usuario.create(req.body)
-    res.json(usuario)
+
+    //extraer los datos 
+    const {email,nombre,password} = req.body
+
+//verificar que el usuario no este duplicado
+const existeUsuario =   await Usuario.findOne({where:{email}})
+if(existeUsuario){
+    return res.render('auth/registro',{
+        pagina:'Crear Cuenta',
+        errores:[{msg:'El usuario ya esta registrado'}],
+        usuario:{
+            nombre:req.body.nombre,
+            email:req.body.email
+        }
+    })
+
+}
+console.log(existeUsuario)
+return;
    
 }
 export {
