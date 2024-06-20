ff6a9285fe35777aa304d394ba24d224ac9e0d02
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-06-20 15:27:50 -0600

    Se agrego la accion para eliminar los datos de precios y categorias y se mostraron desde la base de datos para obtener los precios y categorias

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,3 +1,6 @@
+import Precio from "../models/Precio.js";
+import Categoria from "../models/Categoria.js";
+
 const admin = (req,res)=>{
     res.render('propiedades/admin',{
         pagina:'Mis propiedades',
@@ -6,10 +9,19 @@ const admin = (req,res)=>{
 }
 
 //formulario para crear una nueva propiedad
-const crear = (req,res)=>{
+const crear = async(req,res)=>{
+    //consultar modelo de precio y categorias
+    const [categorias,precios] = await Promise.all([
+        Categoria.findAll(),
+        Precio.findAll()
+    ]);
+
+    //se puede pasar directamente precios y categorias
     res.render('propiedades/crear',{
         pagina:'Crear Propiedad',
-        barra:true
+        barra:true,
+        categorias,
+        precios
     });
 }
 export{
--- seed/seeder.js
+++ seed/seeder.js
@@ -23,8 +23,31 @@ const importarDatos= async ()=>{
         process.exit(1); // terminar los procesos pero hubo un error
     }
 }
+
+const eliminarDatos = async() => {
+    try{
+        //eliminamos los datos con destroy 
+        //reiniciamos el contador de id con truncate
+        // await Promise.all([
+        //     Categoria.destroy({where:{},truncate:true}),
+        //     Precio.destroy({where:{},truncate:true})
+        // ]);
+        await db.sync({force:true});
+        console.log('Datos eliminados correctamente');
+        process.exit();
+
+    }catch(error){
+        console.log(error);
+        process.exit(1); // terminar los procesos pero hubo un error
+    }
+}
  //"db:importar":"node ./seed/seeder.js -i"
     //cuando mande llamar el script se ejecutara este codigo y se tomara el -i (es la posicion 2 del arreglo argv) para hacer la comparacion
     if(process.argv[2] === "-i"){
         importarDatos();
     }
+
+    if(process.argv[2] === "-e"){
+        eliminarDatos();
+    }
+
