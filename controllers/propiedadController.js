8e031d8d94b73d2df7e662835405d1d4cff59a3e
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-06-20 22:09:45 -0600

    Se agrego la validacion del formulario en el router y se agrego el codigo para que se mantengan los datos en el formulario

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,5 +1,6 @@
 import Precio from "../models/Precio.js";
 import Categoria from "../models/Categoria.js";
+import {validationResult}from 'express-validator'
 
 const admin = (req,res)=>{
     res.render('propiedades/admin',{
@@ -20,11 +21,42 @@ const crear = async(req,res)=>{
     res.render('propiedades/crear',{
         pagina:'Crear Propiedad',
         barra:true,
+        csrfToken:req.csrfToken(),
         categorias,
-        precios
+        precios,
+        datos:{} // agregamos datos para que no marque error
     });
+}
+
+const guardar =async (req,res)=>{
+
+    //validacion, aqui se guardaran los errores si es que hay
+    //estos errores vienen del router 
+    let resultado = validationResult(req)
+    //si es diferente de vacio quiere decir que no hay errores
+    if(!resultado.isEmpty()){
+        //si hay error
+    
+        //consultar modelo de precio y categorias
+     const [categorias,precios] = await Promise.all([
+        Categoria.findAll(),
+        Precio.findAll()
+     ]);
+
+        //se puede pasar directamente precios y categorias
+      return   res.render('propiedades/crear',{
+            pagina:'Crear Propiedad',
+            barra:true,
+            csrfToken:req.csrfToken(),
+            categorias,
+            precios,
+            errores:resultado.array(),
+            datos:req.body//esto sirve para mantener los datos en el form y no se pierdan
+        });
+    }
 }
 export{
     admin,
-    crear
+    crear,
+    guardar
 }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,8 +1,22 @@
-import express from "express"
-import { admin, crear } from "../controllers/propiedadController.js"
+import express from "express"  
+import {body} from 'express-validator'
+import { admin, crear,guardar } from "../controllers/propiedadController.js"
 const router = express.Router()
 
 router.get('/mis-propiedades',admin)
 router.get('/propiedades/crear',crear)
+//aqui mismo validamos los campos del formulario 
+router.post('/propiedades/crear',
+    body('titulo').notEmpty().withMessage('El titulo del anuncio es Obligatorio'),
+    body('descripcion').notEmpty().withMessage('La descripcion no puede ir vacia').
+    isLength({max:200}).withMessage("La descripcion es muy larga"),
+    body('categoria').isNumeric().withMessage('Selecciona una categoria'),
+    body('precio').isNumeric().withMessage('Selecciona un rango de precios'),
+    body('habitaciones').isNumeric().withMessage('Selecciona la cantidad de habitaciones'),
+    body('estacionamiento').isNumeric().withMessage('Selecciona la cantidad de estacionamientos'),
+    body('wc').isNumeric().withMessage('Selecciona la cantidad de baños'),
+    body('lat').isNumeric().withMessage('Selecciona la Propiedad en el Mapa')
+    ,guardar)
+
 
 export default router
