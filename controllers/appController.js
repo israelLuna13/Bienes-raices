705d48d47079b6743a706efbae8aaf6042cdbc57
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-06-28 23:19:13 -0600

    Mostramos casas y departamentos en venta debajo del mapa

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,19 +1,51 @@
-import { Categoria,Precio} from '../models/index.js'
+import { Categoria,Precio,Propiedad} from '../models/index.js'
 
 //estas acciones seran para la vista publica o vita general
 
 const inicio =async (req,res)=>{
 
     //hacemos una consulta de las categorias y precios
-    const [categorias,precios] = await Promise.all([
+    const [categorias,precios,casas,departamentos] = await Promise.all([
         Categoria.findAll({raw:true}),
-        Precio.findAll({raw:true})
+        Precio.findAll({raw:true}),
+        //buscamos  3 propiedades con la categoria 1
+        Propiedad.findAll({
+            limit:3,
+            where:{
+                categoriaId:1
+            },
+            include:[
+                {
+                    model:Precio,
+                    as:'precio'
+                }
+            ],
+            //ordenamos por fecha de creacion
+            order:[['createdAt','DESC']]
+        }),
+        //buscamos 3 propiedades con la categoria 2
+        Propiedad.findAll({
+            limit:3,
+            where:{
+                categoriaId:2
+            },
+            include:[
+                {
+                    model:Precio,
+                    as:'precio'
+                }
+            ],
+            //ordenamos por fecha de creacion
+            order:[['createdAt','DESC']]
+        })
     ])
 
     res.render('inicio',{
         pagina:'Inicio',
         categorias,
-        precios
+        precios,
+        casas,
+        departamentos
     })
 }
 
